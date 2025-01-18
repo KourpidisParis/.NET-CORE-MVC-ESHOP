2e9a3aeb555204c4b21400c4fe7e360ea332b1ec
kourpidis_paris <kourpidisparis@gmail.com>
2025-01-18 16:48:00 +0200

    Repository Pattern & Areas

--- Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
+++ Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c134419e3451b31751139a368d587dc256b44d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88eb1fad0b16c2576a107dbd1c3b04d1cdf988a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c134419e3451b31751139a368d587dc256b44d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88eb1fad0b16c2576a107dbd1c3b04d1cdf988a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
